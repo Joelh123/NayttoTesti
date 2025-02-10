88fa523f4fa819830c481890bf7ef72d4dd8870d
Eetu.E <eetueskelinen123@gmail.com>
2025-02-10 12:13:07 +0200

    Add geolocation on load

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+function getUserCoordinates(){
+    map.locate({watch: true, enableHighAccuracy: true})
+    map.on('locationfound', function(e) {
+        let userMarker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
+        stopLocate()
+    });
+
+    map.on('locationerror', function(e) {
+        alert("Location not found")
+    });
+};
+
+getUserCoordinates();
