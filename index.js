b014a13956ddcee66667c128b219f189f91ed37f
Eetu.E <eetueskelinen123@gmail.com>
2025-02-10 12:32:06 +0200

    Add automatically removing old markers upon updating location

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let map = L.map('map').setView([61.687879, 27.273147], 17)
+let userMarker = null
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -9,12 +11,16 @@ function getUserCoordinates(){
     console.log("Locating")
     map.locate({watch: true, enableHighAccuracy: true, timeout: 10000})
     map.on('locationfound', function(e) {
-        let userMarker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
-        map.stopLocate()
+        console.log("Location found")
+        if (userMarker) {
+            userMarker.setLatLng(e.latlng);
+        } else {
+            userMarker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
+        }
     });
 
     map.on('locationerror', function(e) {
-        alert("Location not found")
+        console.log("Location not found")
     });
 };
 
