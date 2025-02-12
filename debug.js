5fa9b7e08f0c8f235de760dbc6bd6a95a08979b5
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-02-12 12:51:01 +0200

    Enhance map functionality with maxBounds and proximity alert for Mannerheim marker

--- debug.js
+++ debug.js
@@ -1,5 +1,7 @@
-let map = L.map('map', { minZoom: 6, maxZoom: 19 }).setView([61.687879, 27.273147], 17)
+let map = L.map('map', { minZoom: 6, maxZoom: 19, maxBounds: [[59, 10],[73, 40]] }).setView([61.687879, 27.273147], 17)
 let marker = L.marker([61.687879, 27.273147], { draggable: true }).addTo(map)
+let mannerheimMarker = L.circle([61.68789288044503, 27.272207983109546], {radius: 10}).addTo(map);
+let thresholdDistance = 20
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,12 +10,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function getMarkerCoordinates(){
     console.log("Locating")
-    map.locate({watch: true, enableHighAccuracy: true, timeout: 15000})
+    map.locate({watch: true, enableHighAccuracy: true, timeout: 2000})
     map.once('locationfound', function(e) {
         console.log("Location found")
         if (marker) {
-            console.log(marker._latlng)
+            console.log(marker.getLatLng())
+            if(map.distance(marker.getLatLng(), mannerheimMarker.getLatLng()) <= thresholdDistance) {
+                window.alert("patsas")
             map.stopLocate()
+            }
         } else {
             map.stopLocate()
             marker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
@@ -27,3 +32,11 @@ function getMarkerCoordinates(){
 
 getMarkerCoordinates();
 setInterval(getMarkerCoordinates, 2000);
+
+// Mannerheim
+// lat
+// : 
+// 61.68789288044503
+// lng
+// : 
+// 27.272207983109546
