a75f215a6103d147d65eaf3c795c0456473ef82c
Eetu.E <eetueskelinen123@gmail.com>
2025-02-12 11:35:41 +0200

    Add possibility to create own icons

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ var bounds = L.latLngBounds(
     L.latLng(59.5, 19.0),
     L.latLng(70.5, 31.5)
 );
+
 map.setMaxBounds(bounds);
 map.on('drag', function() {
     map.panInsideBounds(bounds, { animate: false });
 });
 
+//For custom icon replace Icon with icon and remove iconUrl
+var CustomIcon = L.Icon.extend({
+    options: {
+        iconUrl: L.Icon.Default.prototype._getIconUrl('icon'),
+        shadowUrl: L.Icon.Default.prototype._getIconUrl('shadow'),
+        iconSize: [25, 41],
+        shadowSize: [41, 41],
+        iconAnchor: [12, 41],
+        shadowAnchor: [12, 41],
+        popupAnchor: [1, -34]
+    }
+});
+
+//var customIcon1 = new CustomIcon({iconUrl: 'YOUR ICON IMAGE HERE'}), for custom icon images
+var customIcon1 = new CustomIcon(),
+    customIcon2 = new CustomIcon();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -33,5 +50,9 @@ function getUserCoordinates(){
         console.log("Location not found")
     });
 };
+
+L.marker([61.687879, 27.273147], {icon: customIcon1}).addTo(map).bindPopup("Test icon");
+L.marker([62.687879, 27.273147], {icon: customIcon2}).addTo(map).bindPopup("Test icon");
+
 getUserCoordinates();
 setInterval(getUserCoordinates, 15000);
