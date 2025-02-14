9c6fc7b499090fb9eb219720ead6b7c1628d87d4
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-02-14 11:19:40 +0200

    Add popup when near a landmark

--- debug.js
+++ debug.js
@@ -1,6 +1,6 @@
 let map = L.map('map', { minZoom: 6, maxZoom: 19, maxBounds: [[59, 10],[73, 40]] }).setView([61.687879, 27.273147], 17)
 let marker = L.marker([61.687879, 27.273147], { draggable: true }).addTo(map)
-let mannerheimMarker = L.circle([61.68789288044503, 27.272207983109546], {radius: 10}).addTo(map);
+let mannerheimMarker = { isSet: false, func: L.circle([61.68789288044503, 27.272207983109546], {radius: 10}).addTo(map) }
 let thresholdDistance = 20
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,28 +10,37 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function getMarkerCoordinates(){
     console.log("Locating")
-    map.locate({watch: true, enableHighAccuracy: true, timeout: 2000})
+    map.locate({watch: true, enableHighAccuracy: true, timeout: 10000})
     map.once('locationfound', function(e) {
         console.log("Location found")
         if (marker) {
             console.log(marker.getLatLng())
-            if(map.distance(marker.getLatLng(), mannerheimMarker.getLatLng()) <= thresholdDistance) {
-                window.alert("patsas")
             map.stopLocate()
-            }
         } else {
             map.stopLocate()
             marker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
         }
     });
 
+    displayPopup()
+
     map.on('locationerror', function(e) {
         console.log("Location not found")
     });
 };
 
+function displayPopup() {
+    if(map.distance(marker.getLatLng(), mannerheimMarker.func.getLatLng()) <= thresholdDistance && !mannerheimMarker.isSet) {
+        let popup = L.popup(mannerheimMarker.func.getLatLng(), {content: '<p>Mannerheimin patsas</p>'})
+                    .openOn(map);
+        mannerheimMarker.isSet = true
+    } else if (map.distance(marker.getLatLng(), mannerheimMarker.func.getLatLng()) > thresholdDistance) {
+        mannerheimMarker.isSet = false
+    }
+}
+
 getMarkerCoordinates();
-setInterval(getMarkerCoordinates, 2000);
+setInterval(getMarkerCoordinates, 10000);
 
 // Mannerheim
 // lat
