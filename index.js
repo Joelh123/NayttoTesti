1fb308ccaa9fe4ebf97c4109c4abb3625a6b6f48
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-02-19 09:48:27 +0200

    add popup when near a marker and fix displaying distance

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let map = L.map('map', { minZoom: 6, maxZoom: 19, inertia: true, inertiaDeceleration: 20000 }).setView([61.687879, 27.273147], 17)
 let userMarker = null
+let thresholdDistance = 20
 var bounds = L.latLngBounds(
     L.latLng(59.5, 19.0),
     L.latLng(70.5, 31.5)
@@ -25,8 +26,8 @@ var CustomIcon = L.Icon.extend({
 
 
 let markers = [
-    { marker1: L.marker([61.687879, 27.273147], {icon: new CustomIcon()}).addTo(map).bindPopup("Test icon") },
-    { marker2: L.marker([62.687879, 27.273147], {icon: new CustomIcon()}).addTo(map).bindPopup("Test icon") },
+    { name: L.marker([61.687879, 27.273147], {icon: new CustomIcon()}).addTo(map).bindPopup("Test icon") },
+    { name: L.marker([62.687879, 27.273147], {icon: new CustomIcon()}).addTo(map).bindPopup("Test icon") },
 ]
 
 //let marker1 = L.marker([61.687879, 27.273147], {icon: new CustomIcon()}).addTo(map).bindPopup("Test icon");
@@ -47,6 +48,7 @@ function getUserCoordinates(){
         } else {
             userMarker = L.marker(e.latlng).bindPopup("Your current location").addTo(map);
         }
+        displayPopup()
         checkDistance();
     });  
     };
@@ -58,16 +60,23 @@ function getUserCoordinates(){
 function checkDistance(){
     //alert("Test")
     if (userMarker) {
-        let distance = Math.trunc(userMarker.getLatLng().distanceTo(marker1.getLatLng()));
+        let distance = Math.trunc(userMarker.getLatLng().distanceTo(markers[0].name.getLatLng()));
         if (distance <= 1000){
             console.log(`${distance} meters`);
         } else {
-            console.log(`${distance.toFixed(1)}km`);
+            console.log(`${(distance / 1000).toFixed(1)}km`);
         }
     }
 };
 
-
+function displayPopup() {
+    for (const marker of markers) {
+        if (map.distance(userMarker.getLatLng(), marker.name.getLatLng()) <= thresholdDistance) {
+            L.popup(marker.name.getLatLng(), { content: '<p>Testi</p>', closeButton: false })
+                .openOn(map);
+        }
+    }
+}
 
 getUserCoordinates();
 setInterval(getUserCoordinates, 15000);
